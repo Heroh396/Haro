8677854aeb2aa130e78dc37366edc69726f3a5f6
heroh396 <heroh396@gmail.com>
2017-10-16 10:45:24 +0700

    Add word to voc, add mp3 script to download mp3 file from youtube

--- job/learnVoc.sh
+++ job/learnVoc.sh
@@ -18,11 +18,10 @@ icon="$BASEDIR/../icon/english.png"
 voc="$BASEDIR/../text/voc.txt"
 
 # Command
-notify-send -i $icon "Haro assistant" "$(head -n 1 $voc)"
-if (command -v crontab >/dev/null 2>&1) then
+notify-send -i $icon -t 10000 "Haro assistant" "$(head -n 1 $voc)"
+
+if (command -v xclip >/dev/null 2>&1) then
 	head -n 1 $voc | awk '{print $1;}' | xclip -selection clipboard
-else
-	head -n 1 $voc | awk '{print $1;}'
 fi
 
 awk '{ if ( NR == 1 ) { store=$0 } else { print } }END{ print store }' $voc > tmp.txt
